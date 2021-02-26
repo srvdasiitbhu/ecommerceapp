c220079e246ee609676b6899a5e999f851b9f4dc
srvdasiitbhu <souravdas3298@gmail.com>
2021-02-27 02:27:31 +0530

    LeftNav_Departments

--- src/components/LeftNav/LeftNav.js
+++ src/components/LeftNav/LeftNav.js
@@ -8,7 +8,6 @@ import {
   ListItemIcon,
   Typography,
 } from "@material-ui/core";
-import { ExpandMore } from "@material-ui/icons";
 import FilterListIcon from "@material-ui/icons/FilterList";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import "./LeftNav.css";
@@ -16,13 +15,25 @@ import "./LeftNav.css";
 const LeftNav = () => {
   const [expanded, setExpanded] = useState(false);
   const [open, setOpen] = React.useState(false);
+  const [open1, setOpen1] = React.useState(false);
+  const [open2, setOpen2] = React.useState(false);
+  const [open3, setOpen3] = React.useState(false);
   const toggleExpand = () => setExpanded(!expanded);
   const handleClick = () => {
     setOpen(!open);
   };
+  const handleClick1 = () => {
+    setOpen1(!open1);
+  };
+  const handleClick2 = () => {
+    setOpen2(!open2);
+  };
+  const handleClick3 = () => {
+    setOpen(!open3);
+  };
   return (
     <div>
-      <div>
+      <div class="btn-department">
         <button type="button" className="btn btn-outline-primary">
           <FilterListIcon />
           <Typography className="btn--department">Departments</Typography>
@@ -41,7 +52,7 @@ const LeftNav = () => {
         </ListItem>
         <Collapse in={open} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
-            <ListItem button onClick={handleClick}>
+            <ListItem button onClick={handleClick1}>
               <ListItemIcon />
               <ArrowForwardIosIcon
                 // className={clsx(classes.expand, expanded && classes.expandOpen)}
@@ -52,9 +63,10 @@ const LeftNav = () => {
               />
               <Typography>Electronics</Typography>
             </ListItem>
-            <Collapse in={open} timeout="auto" unmountOnExit>
+            <Collapse in={open1} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
                 <ListItem button onClick={handleClick}>
+                  <ListItemIcon />
                   <ListItemIcon />
                   <ArrowForwardIosIcon
                     // className={clsx(classes.expand, expanded && classes.expandOpen)}
@@ -63,12 +75,95 @@ const LeftNav = () => {
                     aria-expanded={expanded}
                     aria-label="show more"
                   />
-                  <Typography>Electronics</Typography>
+                  <Typography>Cell Phones & Smartphones</Typography>
                 </ListItem>
+              </List>
+            </Collapse>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItem button onClick={handleClick1}>
+                  <ListItemIcon />
+                  <ArrowForwardIosIcon
+                    // className={clsx(classes.expand, expanded && classes.expandOpen)}
+                    className="leftnav-arrow"
+                    fontSize="small"
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                  />
+                  <Typography>Business & Industrial</Typography>
+                </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    <ListItem button onClick={handleClick1}>
+                      <ListItemIcon />
+                      <ArrowForwardIosIcon
+                        // className={clsx(classes.expand, expanded && classes.expandOpen)}
+                        className="leftnav-arrow"
+                        fontSize="small"
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                      />
+                      <Typography>Computers</Typography>
+                    </ListItem>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                      <List component="div" disablePadding>
+                        <ListItem button onClick={handleClick1}>
+                          <ListItemIcon />
+                          <ArrowForwardIosIcon
+                            // className={clsx(classes.expand, expanded && classes.expandOpen)}
+                            className="leftnav-arrow"
+                            fontSize="small"
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                          />
+                          <Typography>Consumer Electronics</Typography>
+                        </ListItem>
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                          <List component="div" disablePadding>
+                            <ListItem button onClick={handleClick1}>
+                              <ListItemIcon />
+                              <ArrowForwardIosIcon
+                                // className={clsx(classes.expand, expanded && classes.expandOpen)}
+                                className="leftnav-arrow"
+                                fontSize="small"
+                                aria-expanded={expanded}
+                                aria-label="show more"
+                              />
+                              <Typography>Home & Garden</Typography>
+                            </ListItem>
+                            <Collapse in={open} timeout="auto" unmountOnExit>
+                              <List component="div" disablePadding>
+                                <ListItem button onClick={handleClick1}>
+                                  <ListItemIcon />
+                                  <ArrowForwardIosIcon
+                                    // className={clsx(classes.expand, expanded && classes.expandOpen)}
+                                    className="leftnav-arrow"
+                                    fontSize="small"
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                  />
+                                  <Typography>Collectibles</Typography>
+                                </ListItem>
+                              </List>
+                            </Collapse>
+                          </List>
+                        </Collapse>
+                      </List>
+                    </Collapse>
+                  </List>
+                </Collapse>
               </List>
             </Collapse>
           </List>
         </Collapse>
+      </List>
+      <List>
+        <ListItem button onClick={handleClick}>
+          <Typography className="leftnav-categories">
+            Collapsed Filters
+          </Typography>
+          <IconButton aria-expanded={expanded} aria-label="show more" />
+        </ListItem>
       </List>
     </div>
   );
